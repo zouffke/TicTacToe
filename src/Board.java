8ad9f0c1ba9ba2440935e9f730fcc2a6c0d22377
Luca.Dand <Luca.dandois@student.kdg.be>
2022-12-13 13:34:31 +0100

    worked on Board.java class. Vraag gesteld aan Maarten of het beter zou zijn om naar een Array over te schakelen in de plaats van een List

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
 
-    private static final int LENGTH;
-    private static final int WIDTH;
+    private static final int LENGTH = 3;
+    private static final int WIDTH = 3;
+    private List<List<Piece>> pieces;
 
+    public Board(){
+        pieces = new ArrayList<>();
+    }
+
+    public boolean place(int x, int y, Piece piece){
+        if (x > WIDTH){
+            System.out.println("Dit vak bestaat niet");
+            return false;
+        } else if (y > LENGTH) {
+            System.out.println("Dit vak bestaat niet");
+            return false;
+        } else if (pieces.get(x - 1).get(y - 1) != null){
+            System.out.println("Dit vak is a bezet");
+            return false;
+        } else{
+            pieces.get(x - 1).add(y - 1, piece);
+            return true;
+        }
+    }
+
+    public boolean win(){
+        int count = 0;
+    }
 }
--- src/Piece.java
+++ src/Piece.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public abstract class Piece {
 
     private Sort sort;
@@ -6,7 +8,7 @@ public abstract class Piece {
     private static int iD = 0;
     private int id;
 
-    public Piece(Sort sort, int x, int y){
+    public Piece(Sort sort, int x, int y) {
         this.x = x;
         this.y = y;
         this.sort = sort;
@@ -31,5 +33,22 @@ public abstract class Piece {
 
     public int getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Piece piece = (Piece) o;
+        return x == piece.x && y == piece.y && id == piece.id && sort == piece.sort;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sort, x, y, id);
+    }
+
+    public boolean equalsSort(Sort sort){
+        return this.getSort().equals(sort);
     }
 }
