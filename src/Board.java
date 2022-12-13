8b125c356f2693f3c9a24024da31c8999248b64a
Luca.Dand <Luca.dandois@student.kdg.be>
2022-12-13 12:34:36 +0100

    Board variabels made

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,6 @@
 public class Board {
+
+    private static final int LENGTH;
+    private static final int WIDTH;
+
 }
--- src/Piece.java
+++ src/Piece.java
@@ -10,5 +10,26 @@ public abstract class Piece {
         this.x = x;
         this.y = y;
         this.sort = sort;
+        this.id = iD++;
+    }
+
+    public Sort getSort() {
+        return sort;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public static int getiD() {
+        return iD;
+    }
+
+    public int getId() {
+        return id;
     }
 }
