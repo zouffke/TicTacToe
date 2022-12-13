dc91dfbd57c09612a55b90bb1ea9bea1f2d135c8
Luca.Dand <Luca.dandois@student.kdg.be>
2022-12-13 12:24:31 +0100

    Enum Sort filled in, Class Piece constructor written

--- src/Piece.java
+++ src/Piece.java
@@ -1,3 +1,14 @@
 public abstract class Piece {
-    int X, Y;
+
+    private Sort sort;
+    private int x;
+    private int y;
+    private static int iD = 0;
+    private int id;
+
+    public Piece(Sort sort, int x, int y){
+        this.x = x;
+        this.y = y;
+        this.sort = sort;
+    }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -1,2 +1,3 @@
 public enum Sort {
+    X, O;
 }
