d9f315cf0c7b8db6eb82702cdc0cf2e32109a2b3
Luca.Dand <Luca.dandois@student.kdg.be>
2022-12-16 18:04:59 +0100

    The game is fully optimized for the support of bigger boards

--- src/Game.java
+++ src/Game.java
@@ -4,8 +4,9 @@ public class Game {
     public static void main(String[] args) {
         Contribution contribution;
         Scanner keyboard = new Scanner(System.in);
-        Board board = new Board(9, 9);
+        Board board;
 
+        board = setBoard(keyboard);
 
         contribution = getPlayers(keyboard);
 
@@ -13,11 +14,35 @@ public class Game {
 
     }
 
+    public static Board setBoard(Scanner keyboard){
+        int choice;
+
+        System.out.print("Kies het soort bord dat u wilt gebruiken:\n1: 3x3\n2: 6x6\n3: 9x9\n\tKeuze: ");
+        choice = keyboard.nextInt();
+
+        switch (choice) {
+            case 1 -> {
+                return new Board(3, 3);
+            }
+            case 2 -> {
+                return new Board(6, 6);
+            }
+            case 3 -> {
+                return new Board(9, 9);
+            }
+            default -> {
+                System.out.println("Deze keuze bestaat niet.\nStandaard bord van 3x3 wordt gebruikt.");
+                return new Board(3, 3);
+            }
+        }
+    }
+
     public static Contribution getPlayers(Scanner keyboard) {
         String name1;
         String name2;
 
         do {
+            keyboard.nextLine();
             System.out.print("Geef de naam van speler 1: ");
             name1 = keyboard.nextLine();
         } while (name1.isEmpty() || !Character.isAlphabetic(name1.charAt(0)));
@@ -31,7 +56,7 @@ public class Game {
 
     public static void play(Contribution contribution, Board board, Scanner keyboard) {
         int count = 1;
-        boolean validMove = false;
+        boolean validMove;
         String input;
 
         System.out.printf("%s speelt met %s\n", contribution.getName(1), contribution.getSort(1));
@@ -48,10 +73,10 @@ public class Game {
             }
 
             do {
+                validMove = false;
                 System.out.print("Waar wilt u een stuk plaatsen?\nLocatie: ");
                 input = keyboard.nextLine();
-
-                if (!input.isEmpty() || !input.matches("[0-9]" + "-" + "[0-9]")){
+                if (!input.isEmpty() && input.matches("[0-9]" + "-" + "[0-9]")){
                     validMove = true;
                 }
 
