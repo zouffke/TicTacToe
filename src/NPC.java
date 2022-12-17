9c1f864bbbc439d9066d00d7b0114cbaabffc9d4
quinten_maas <quinten.maas@student.kdg.be>
2022-12-17 21:31:03 +0100

    NPC aangemaakt

--- src/NPC.java
+++ src/NPC.java
@@ -1,2 +1,10 @@
 public class NPC {
+    private static String name;
+    public NPC (String name){
+        NPC.name = name;
+    }
+
+    public static void setName(String name) {
+        NPC.name = name;
+    }
 }
