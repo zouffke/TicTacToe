cd0aaa4a65373c22e5a03a04f1faaaf1fe4abe6b
Luca.Dand <Luca.dandois@student.kdg.be>
2022-12-20 16:46:02 +0100

    added a function to check the corners of the board

--- src/NPC.java
+++ src/NPC.java
@@ -15,9 +15,9 @@ public class NPC extends Player {
         Random random = new Random();
         Sort sort;
 
-        if (count == 1){
+        if (count == 1) {
             sort = Sort.X;
-        } else{
+        } else {
             sort = Sort.O;
         }
 
@@ -44,29 +44,43 @@ public class NPC extends Player {
 
     private boolean smartNPC(Board board, Random random, Sort sort) {
         Piece[][] pieces = board.getPieces();
+
+        return corners(pieces, random, board);
+    }
+
+    private boolean corners(Piece[][] pieces, Random random, Board board){
         int size = Board.getWidth();
         int sizeArray = size - 1;
+        String index = "";
 
+        //pieces in the corners?
         if (pieces[0][0] != null || pieces[0][sizeArray] != null || pieces[sizeArray][0] != null || pieces[sizeArray][sizeArray] != null) {
-            if (size == 3 && pieces[1][1] == null) {
-                board.place("2-2", false);
-                return true;
+            //Yes;
+            //Block the piece in the corner by placing next to it
+            if (pieces[0][0] != null && pieces[1][1] == null) {
+                index = String.format("%d-%d", 2, 2);
+            } else if (pieces[0][sizeArray] != null && pieces[1][sizeArray - 1] == null) {
+                index = String.format("%d-%d", 2, size - 1);
+            } else if (pieces[sizeArray][0] != null && pieces[sizeArray - 1][1] == null) {
+                index = String.format("%d-%d", size - 1, 2);
+            } else if (pieces[sizeArray][sizeArray] != null && pieces[sizeArray - 1][sizeArray - 1] == null){
+                index = String.format("%d-%d", size - 1, size - 1);
             }
+            //No;
         } else {
-            String index = "";
+            //Pick a random corner to place a piece
             switch (random.nextInt(4) + 1) {
                 case 1 -> index = "1-1";
                 case 2 -> index = String.format("1-%d", size);
                 case 3 -> index = String.format("%d-1", size);
                 case 4 -> index = String.format("%d-%d", size, size);
             }
+        }
+
+        if (!index.isEmpty()) {
+            System.out.printf("%s speelde: %s\n", getNAME(), index);
             board.place(index, false);
             return true;
-        }
-        if (board.win(Sort.oppositSort(sort), true)){
-
-        } else if (board.win(sort, true)){
-
         }
         return false;
     }
